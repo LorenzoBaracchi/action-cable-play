6624949e4f461c8fb70c7655bf046c838f955ec1
LorenzoBaracchi <baracchi.lorenzo@gmail.com>
2016-08-24 14:40:27 +0200

    redis configuration for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.web_socket_server_url = "wss://action-cable-play.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://action-cable-play.herokuapp.com', 'http://action-cable-play.herokuapp.com']
 end
